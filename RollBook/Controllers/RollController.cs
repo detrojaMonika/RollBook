ef4c454f594c036171920b723ba558ddc15874ab
mitanshiwebsmith <mitanshi@websmithsolution.com>
2024-03-14 10:39:05 +0530

    --Change in RollController & Roll_Dal.cs

--- RollBook/Controllers/RollController.cs
+++ RollBook/Controllers/RollController.cs
@@ -17,7 +17,8 @@ namespace RollBook.Controllers
     {
 
         Roll_DAL _RollDAL = new Roll_DAL();
-        string conString = ConfigurationManager.ConnectionStrings["Student_InformationConnectionstring"].ToString();
+        //string conString = ConfigurationManager.ConnectionStrings["Student_InformationConnectionstring"].ToString();
+        string conString = ConfigurationManager.ConnectionStrings["DefaultConnection"].ToString();
 
         // GET: Student
         public ActionResult Index()
